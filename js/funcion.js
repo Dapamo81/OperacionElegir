62af8e84e8899512b030bc83ef9afdf47d2a59c2
Dani <dpajaresfo@gmail.com>
2025-04-17 20:09:48 +0200

    Añadimos la funcion calcular

--- js/funcion.js
+++ js/funcion.js
@@ -3,3 +3,20 @@ function sumar(a,b){
     mostrarResultado("La suma es:" + res);
 }
 
+function calcular(){
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    const oper = document.getElementById("operacion").value.trim();
+
+    if(isNaN(num1) || isNaN(num2)){
+        mostrarResultado("Por favor, ingresa números válidos.");
+        return;
+    }
+
+    if(oper === "+"){
+        sumar(num1,num2);
+    }else{
+        mostrarResultado("Operación no válida. Usa + o -.")
+    }
+
+}
