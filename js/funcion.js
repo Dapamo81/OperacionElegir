5231057c7106d4f1ce02acaa79853f9819cce059
Dani <dpajaresfo@gmail.com>
2025-04-17 20:21:26 +0200

    añadimos el resultado con returns

--- js/funcion.js
+++ js/funcion.js
@@ -1,10 +1,12 @@
 function sumar(a,b){
     const res = a+b;
-    mostrarResultado("La suma es: " + res);
+    // mostrarResultado("La suma es: " + res);
+    return res;
 }
 function restar(a,b){
     const res = a-b;
-    mostrarResultado("La resta es: " + res);
+    // mostrarResultado("La resta es: " + res);
+    return res;
 }
 
 function mostrarResultado(mensaje) {
@@ -20,11 +22,21 @@ function calcular(){
         mostrarResultado("Por favor, ingresa números válidos.");
         return;
     }
+    
+    let resultado;
 
     if(oper === "+"){
-        sumar(num1,num2);
+        //sumar(num1,num2);
+
+        resultado = sumar(num1,num2);
+        mostrarResultado("La suma es: " + resultado)
+
     }else if (oper === "-"){
-        restar(num1,num2);
+        // restar(num1,num2);
+
+        resultado = restar(num1,num2);
+        mostrarResultado("La resta es: " + resultado)
+
     }else{
         mostrarResultado("Operación no válida. Usa + o -.")
     }
